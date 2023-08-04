975a518cc705114f6f94ae85cf68e4f6fab4b701
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-08-04 13:49:57 +0530

    Attendence

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Console.WriteLine(" Check whether Employee is present or absent");
+                int empPresesent = 1;
+                Random random = new Random();
+                int empCheck = random.Next(2);
+                if (empCheck == empPresesent)
+                {
+                    Console.WriteLine("Employee is Present");
+                }
+                else
+                    Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+    
